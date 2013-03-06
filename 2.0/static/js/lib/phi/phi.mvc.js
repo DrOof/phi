0ff5db53e7616305889d27079815a55579161a9a
DrOof <olivierhermanus@gmail.com>
2013-03-06 11:38:42 +0100

    added MCV basic implementation.

--- 2.0/static/js/lib/phi/phi.mvc.js
+++ 2.0/static/js/lib/phi/phi.mvc.js
@@ -18,25 +18,48 @@
     var Observable = phi({
         
         __init__: function() {
+            
             this.listeners = {};
-        },
-        
-        addEventListener: function( type, listener ) {
             
         },
         
-        removeEventListener: function( type, listener ) {
+        addEventListener: function( type, fn ) {
+            
+
+            
+            var fns = this.listeners[ type ] || [];
+            
+            if ( fns.indexOf( fn ) === -1 ) {
+                fns.push( fn );
+            }
+            
+            this.listeners[ type ] = fns;
+            
+        },
+        
+        removeEventListener: function( type, fn ) {
             
         },
         
         dispatchEvent: function( e ) {
+            
+            var fns = this.listeners[ e.type ] || [];
+            
+            for (var i = fns.length - 1; i >= 0; i--){
+                fns[ i ]( e );
+            };
             
         }
         
     });
     
     
-    
+	
+    /*
+     * Model
+     *
+     *
+     */
 
     var Model = phi.mvc.Model = phi({
     
@@ -44,13 +67,14 @@
     
         __init__: function() {
             this.super();
+            this.__data__ = {};
         },
         
         change: function( path, value ) {
         
             this.__data__[ path ] = value;
             this.dispatchEvent( { type: 'modelchange', target: this.__data__[ path ], path: path, data: this.__data__ } );
-              
+            
         },
         
         save: function() {
@@ -60,7 +84,11 @@
     });
     
     
-    
+    /*
+     * View
+     *
+     *
+     */
     
     var View = phi.mvc.View = phi({
         
@@ -71,26 +99,32 @@
         },
         
         update: function( path, target ) {
-            
+            console.log( path, target );
         }
         
     });
     
     
     
+    /*
+     * Controller
+     *
+     *
+     */
     
     var Controller = phi.mvc.Controller = phi({
     
-        __init__: function() {
+        __init__: function( view, model ) {
             
-            this.view = new View();
-            this.model = new Model();
+            this.view = view;
+            this.model = model;
             
-            this.model.addEventListener('modelchange', this.handleModelUpdate.bind( this ) );
+            this.model.addEventListener( 'modelchange', this.handleModelChange.bind( this ) );
             
         },
         
         handleModelChange: function( e ) {
+            console.log( 'handleModelChange', e );
             this.view.update( e.path, e.target );
         }
     
