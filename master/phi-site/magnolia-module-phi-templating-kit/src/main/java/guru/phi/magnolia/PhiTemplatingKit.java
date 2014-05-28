e19937dc92057c2527c0f61826a52015453aabe2
DrOof <olivierhermanus@gmail.com>
2014-05-28 10:39:35 +0200

    Reorganised phi.

---
+++ master/phi-site/magnolia-module-phi-templating-kit/src/main/java/guru/phi/magnolia/PhiTemplatingKit.java
@@ -0,0 +1,12 @@
+package guru.phi.magnolia;
+
+/**
+ * This class is optional and represents the configuration for the magnolia-module-phi-templating-kit module.
+ * By exposing simple getter/setter/adder methods, this bean can be configured via content2bean
+ * using the properties and node from <tt>config:/modules/magnolia-module-phi-templating-kit</tt>.
+ * If you don't need this, simply remove the reference to this class in the module descriptor xml.
+ */
+public class PhiTemplatingKit {
+    /* you can optionally implement info.magnolia.module.ModuleLifecycle */
+
+}
