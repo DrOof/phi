822f2ad0af32dd859eccc29fa726cdb02f6b59b4
DrOof <olivierhermanus@gmail.com>
2014-07-28 16:12:15 +0200

    Using jetty for parodying static resources.

--- master/phi-site/phi-templating-kit/src/main/resources/phi-templating-kit/sync.sh
+++ master/phi-site/phi-templating-kit/src/main/resources/phi-templating-kit/sync.sh
@@ -1,2 +1,2 @@
-# this script is a shorthand for rsyncing resources to the live webapp when using "mvn tomcat7:run"
+# this script is a shorthand for rsyncing resources to the live webapp when using "mvn tomcat7:run"aaa
 rsync -r * ../../../../../target/tomcat/webapps/phi-webapp/phi-templating-kit/
