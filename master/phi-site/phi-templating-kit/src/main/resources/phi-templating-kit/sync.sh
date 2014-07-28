7a97227480c02194f8287e792c122a7b9d1ed2c5
DrOof <olivierhermanus@gmail.com>
2014-07-28 16:15:50 +0200

    Added TODO.

--- master/phi-site/phi-templating-kit/src/main/resources/phi-templating-kit/sync.sh
+++ master/phi-site/phi-templating-kit/src/main/resources/phi-templating-kit/sync.sh
@@ -1,2 +1,10 @@
-# this script is a shorthand for rsyncing resources to the live webapp when using "mvn tomcat7:run"aaa
+#
+# This script is a shorthand for rsyncing resources 
+# to the live webapp when using "mvn tomcat7:run".
+#
+# TODO :    fix linking to static resources through 
+#           the maven tomcat7 plugin.
+#
+#
+
 rsync -r * ../../../../../target/tomcat/webapps/phi-webapp/phi-templating-kit/
