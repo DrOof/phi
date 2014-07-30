7ee8fdb7ad715fc3aacc7bc7d6735912e98f8162
DrOof <olivierhermanus@gmail.com>
2014-07-30 11:34:33 +0200

    Forgot to check in..

--- master/phi-parent/phi-js/phi-js-dom/src/main/resources/phi-js/static/js/lib/phi/phi.dom.js
+++ master/phi-parent/phi-js/phi-js-dom/src/main/resources/phi-js/static/js/lib/phi/phi.dom.js
@@ -18,6 +18,41 @@
         
     };
     
+    /**
+     *
+     * SVGShapeElement
+     *
+     */
+    
+    var SVGNS = 'http://www.w3.org/2000/svg';
+    var SVGShapeElement = dom.SVGShapeElement = phi({
+    
+        __init__ : function( element, attributes ) {
+        
+            this.element = ( typeof element === 'string' ) ? document.createElementNS( SVGNS, element ) : element;
+            this.attr( attributes || {} )
+        
+        },
+    
+        attr: function( attributes ) {
+            
+            for( var key in attributes ) {
+                
+                this.element[key] = attributes[ key ];
+                this.element.setAttribute( key, attributes[ key ] );
+                
+            }
+        
+        },
+        
+        appendChild: function( svg ) {
+            
+            this.element.appendChild( svg.element )
+            
+        }
+    
+    });
+    
     
     /**
      *
---
+++ master/phi-parent/phi-js/phi-js-graph/src/main/resources/phi-js/static/js/lib/phi/phi.graph.js
@@ -0,0 +1,77 @@
+( function( dom ) {
+    
+    var graph = phi.graph || {};
+    
+    var Graph = phi.graph.Graph = phi({
+        
+        __extends__ : phi.EventTarget,
+        
+        __init__ : function() {
+            
+        },
+        
+        update: function( data ) {
+            
+        },
+        
+        render: function() {
+            
+        }
+        
+    });
+    
+    var PieGraph = phi.graph.PieGraph = phi({
+        
+        __extends__ : phi.graph.Graph,
+        
+        __init__ : function() {
+            
+        },
+        
+        update: function( data ) {
+            
+        },
+        
+        render: function() {
+            
+        }
+        
+    });
+    
+    var BarGraph = phi.graph.BarGraph = phi({
+        
+        __extends__ : phi.graph.Graph,
+        
+        __init__ : function() {
+            
+        },
+        
+        update: function( data ) {
+            
+        },
+        
+        render: function() {
+            
+        }
+        
+    });
+    
+    var LineGraph = phi.graph.LineGraph = phi({
+        
+        __extends__ : phi.graph.Graph,
+        
+        __init__ : function() {
+            
+        },
+        
+        update: function( data ) {
+            
+        },
+        
+        render: function() {
+            
+        }
+        
+    });
+    
+} )( phi.dom );
