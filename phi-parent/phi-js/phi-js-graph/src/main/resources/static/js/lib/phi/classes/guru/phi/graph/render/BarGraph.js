8522f061a5e2e478417d80328406e105126c9541
DrOof <olivierhermanus@gmail.com>
2014-12-23 18:06:02 +0100

    Moving Graph renderer to render

---
+++ phi-parent/phi-js/phi-js-graph/src/main/resources/static/js/lib/phi/classes/guru/phi/graph/render/BarGraph.js
@@ -0,0 +1,231 @@
+/**
+ *
+ * Phi Core - A multi-paradigm JavaScript library
+ *
+ * // Externalized source: javascript/phi/src/main/javascript
+ *
+ * Copyright (c) 2010 Authors of PHI
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ *
+ */
+
+( function( dom ) {
+    
+    var graph = phi.graph = phi.graph || {};
+    
+    var BarGraph = phi.graph.BarGraph = phi({
+        
+        __extends__ : phi.graph.Graph,
+        
+        __applies__ : phi.graph.Renderable,
+        
+        __init__ : function( node, options ) {
+            
+            this.__options__ = phi.extend( BarGraph.DEFAULTS, options );
+            
+        },
+        
+        render: function( data ) {
+            
+            var values = this.resolveValuesByName( this.__options__[ 'axis-y-name' ] );
+            var colors = this.resolveColorRange( this.__options__[ 'point-color' ], this.__options__[ 'point-color-shift' ] );
+            
+            var range = this.resolveRangeY( data );
+            var interval = this.resolveAxisInterval( range.delta );
+            
+            range = this.stretchRangeToFit( range, interval );
+            
+            var d = this.resolveCanvasDimensions();
+            var p = [ 40, 40, 40, 80 ];
+            
+            var w = this.resolvePointWidth( d, p, data.length, this.__options__[ 'point-width' ] );
+            
+            this.renderAxisY( d, p, range, interval );
+            
+            for ( var n = 0; n < data.length; n++ ) {
+                this.renderPoint( data[n], range, values, d, p, w, n, colors[n] );
+            }
+            
+        },
+        
+        renderPoint: function( point, range, values, d, p, w, n, c ) {
+            
+            // step
+            
+            var s = ( ( d.width - p[1] - p[3] ) / values.length ); 
+            
+            var v = this.resolveValueY( point );
+            
+            // w = width divided by number of points
+            
+            var h = ( ( d.height - p[0] - p[2] ) / range.max ) * v;
+            var x = ( ( s * n ) ) + ( s / 2 ) + p[3];
+            var y = d.height - h - p[2];
+            
+            this.renderPointShape( point, x, y, w, h, n, c );
+            
+        },
+        
+        renderPointShape: function( point, x, y, w, h, n, c ) {
+            
+            var rect = new phi.dom.svg.SVGShapeElement( 'rect' );
+            this.processSVGShapeElement( rect );
+            
+            rect.attr( { x : x - ( w / 2 ), y : y, width : w, height : h, fill : c } );
+            rect.attr( { 'class' : 'graph-point graph-point-' + n, point : point } );
+            
+            this.__canvas__.appendChild( rect );
+            
+        },
+        
+        /**
+         *
+         * FIXME : Refactor code.
+         * 
+         */
+        
+        renderAxisY: function( d, p, range, i ) {
+            
+            var w = d.width - p[1] - p[3];
+            var h = d.height - p[0] - p[2];
+            
+            var line = new phi.dom.svg.SVGShapeElement( 'line' );
+            line.attr( { x1 : p[3], y1: p[0], x2 : p[3], y2 : h + p[0] } );
+            line.attr( { 'class' : 'graph-axis graph-axis-y' } );
+            
+            this.__canvas__.appendChild( line );
+            
+            var l = range.delta / i;
+            var s = h / l;
+            var m = 0; // range.min;
+            
+            for ( var n = 0; n < l+1; n++) {
+                this.renderAxisYInterval( p, w, h, l, s, i, n, m );
+            }
+            
+        },
+        
+        /**
+         *
+         * FIXME : refactor
+         * FIXME : deal with large numbers on intervals
+         *
+         */
+        
+        renderAxisYInterval: function( p, w, h, l, s, i, n, m ) {
+            
+            var x1 = p[3],
+                y1 = p[0] + h - ( n * s ),
+                x2 = p[3] + w,
+                y2 = y1;
+            
+            var line = new phi.dom.svg.SVGShapeElement( 'line' );
+            line.attr( { x1 : x1, y1 : y1, x2 : x2, y2: y2 } );
+            line.attr( { 'class' : 'graph-axis-interval graph-axis-interval-' + n } );
+            
+            this.__canvas__.appendChild( line );
+            
+            var text = new phi.dom.svg.SVGShapeElement( 'text' );
+            text.attr( { x : x1 - 20, y : y2 + 3, textContent : m + ( n * i ) } );
+            text.attr( { 'class' : 'graph-axis-interval-text graph-axis-y-interval-text' } );
+            
+            this.__canvas__.appendChild( text );
+            
+        },
+        
+        resolvePointWidth : function( d, p, s, option ) {
+            var max = ( d.width - p[1] - p[3] ) / s;
+            return option === 'auto' ? max : ( option > max ? max : option );
+        },
+        
+        /**
+         *
+         * FIXME : Refactor code.
+         * 
+         */
+        
+        resolveAxisInterval : function( delta, exponent, closest, factor ) {
+            
+            var optimal = 10;
+            var proper = [ 1, 2, 5 ];
+            
+            exponent = exponent || 1;
+            closest  = closest || Infinity;
+            
+            var f, p;
+            for ( var i = 0; i < proper.length; i++ ) {
+                
+                f = proper[ i ] * exponent;
+                p = this.resolveAxisIntervalProximity( optimal, delta / f );
+                
+                if ( p < closest ) {
+                    closest = p;
+                    factor = f;
+                } else {
+                    return factor;
+                }
+                
+            }
+            
+            return this.resolveAxisInterval( delta, exponent * 10, closest, factor );
+            
+        },
+        
+        /**
+         *
+         * FIXME : move to parent
+         *
+         */
+        
+        stretchRangeToFit : function( range, interval ) {
+            
+            var min = Math.floor( range.min / interval ) * interval, 
+                max = Math.ceil( range.max / interval ) * interval;
+            
+            return {
+                min : min, max : max,
+                delta : max - min
+            }
+            
+        },
+        
+        /**
+         *
+         * FIXME : move to parent
+         *
+         */
+        
+        resolveAxisIntervalProximity : function( optimal, real ) {
+            return Math.abs( optimal - real );
+        }
+        
+    });
+    
+    BarGraph.DEFAULTS = {
+        'axis-x-name'       : undefined,
+        'axis-x-interval'   : 10,
+        'point-width'       : 'auto',
+        'point-color'       : '#ff0099',
+        'point-color-shift' : 0,
+        'canvas-padding'    : [ 40, 40, 40, 40 ]
+    };
+    
+} )( phi.dom );
---
+++ phi-parent/phi-js/phi-js-graph/src/main/resources/static/js/lib/phi/classes/guru/phi/graph/render/LineGraph.js
@@ -0,0 +1,299 @@
+/**
+ *
+ * Phi Core - A multi-paradigm JavaScript library
+ *
+ * // Externalized source: javascript/phi/src/main/javascript
+ *
+ * Copyright (c) 2010 Authors of PHI
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ *
+ */
+
+( function( dom ) {
+    
+    var graph = phi.graph = phi.graph || {};
+    
+    var LineGraph = phi.graph.LineGraph = phi({
+        
+        __extends__ : phi.graph.Graph,
+        
+        __applies__ : phi.graph.Renderable,
+        
+        __init__ : function( node, options ) {
+            
+            this.__options__ = phi.extend( LineGraph.DEFAULTS, options );
+            
+        },
+        
+        render: function( data ) {
+            
+            var sorted = this.resolveSortOrderByName( data, this.__options__[ 'axis-x-name' ] );
+            var colors = this.resolveColorRange( this.__options__[ 'point-color' ], this.__options__[ 'point-color-shift' ] );
+            
+            var rx = this.resolveRangeX( data );
+            var ry = this.resolveRangeY( data );
+            
+            var ix = this.resolveAxisInterval( rx.delta );
+            var iy = this.resolveAxisInterval( ry.delta );
+            
+            rx = this.stretchRangeToFit( rx, ix );
+            ry = this.stretchRangeToFit( ry, iy );
+            
+            var d = this.resolveCanvasDimensions();
+            
+            this.renderAxisX( d, rx, ix );
+            this.renderAxisY( d, ry, iy );
+            
+            var p0 = null;
+            var p1 = null;
+            var point = null;
+            for ( var n = 0; n < sorted.length; n++ ) {
+                
+                point = sorted[n];
+                
+                p0 = this.resolvePointPosition( point, rx, ry, d );
+                
+                this.renderPointCircle( point, p0.x, p0.y, n, colors[n] );
+                if ( p0 && p1 ) {
+                    this.renderPointToPointLine( p0.x, p0.y, p1.x, p1.y, colors[n] );
+                }
+                
+                // shift p0 to p1
+                p1 = p0;
+                
+            }
+            
+        },
+        
+        renderPointCircle: function( point, x, y, n, color ) {
+            
+            var circle = new phi.dom.svg.SVGShapeElement( 'circle' );
+            this.processSVGShapeElement( circle );
+            
+            circle.attr( { cx : x, cy : y, r : 5, fill : color } );
+            circle.attr( { 'class' : 'graph-point graph-point-' + n, point : point } );
+            
+            this.__canvas__.appendChild( circle );
+            
+        },
+        
+        renderPointToPointLine: function( x1, y1, x2, y2, color ) {
+            
+            var line = new phi.dom.svg.SVGShapeElement( 'line' )
+            line.attr( { x1 : x1, y1 : y1, x2 : x2, y2 : y2, stroke : color } );
+            
+            this.__canvas__.appendChild( line );
+            
+        },
+        
+        resolvePointPosition: function( point, rx, ry, d ) {
+            
+            // top, right, bottom, left
+            var p = [ 40, 40, 40, 40 ];
+            
+            var w = d.width - p[1] - p[3];
+            var h = d.height - p[0] - p[2];
+            
+            var vx = this.resolveValueX( point );
+            var vy = this.resolveValueY( point );
+            
+            var x = ( w / rx.delta ) * ( vx - rx.min ) + p[3];
+            var y = ( h / ry.delta ) * ( vy - ry.min ) + p[0];
+            
+            return { x : x, y: y };
+            
+        },
+        
+        /**
+         *
+         * FIXME : Refactor code.
+         * 
+         */
+        
+        renderAxisX: function( d, range, i ) {
+            
+            var p = [ 40, 40, 40, 40 ];
+            
+            var w = d.width - p[1] - p[3];
+            var h = d.height - p[0] - p[2];
+            
+            var line = new phi.dom.svg.SVGShapeElement( 'line' );
+            line.attr( { x1 : p[3], y1: p[0] + h, x2 : p[3] + w, y2 : p[0] + h } );
+            line.attr( { 'class' : 'graph-axis graph-axis-x' } );
+            
+            this.__canvas__.appendChild( line );
+            
+            var l = range.delta / i;
+            var s = w / l;
+            var m = range.min;
+            
+            for ( var n = 0; n < l+1; n++) {
+                this.renderAxisXInterval( p, w, h, l, s, i, n, m );
+            }
+            
+        },
+        
+        /**
+         *
+         * FIXME : refactor
+         * FIXME : deal with large numbers on intervals
+         *
+         */
+        
+        renderAxisXInterval: function( p, w, h, l, s, i, n, m ) {
+            
+            var x1 = p[3] + ( n * s ),
+                y1 = p[0] + h,
+                x2 = x1,
+                y2 = p[0];
+            
+            var line = new phi.dom.svg.SVGShapeElement( 'line' );
+            line.attr( { x1 : x1, y1 : y1, x2 : x2, y2: y2 } );
+            line.attr( { 'class' : 'graph-axis-interval graph-axis-interval-' + n } );
+            
+            this.__canvas__.appendChild( line );
+            
+            var text = new phi.dom.svg.SVGShapeElement( 'text' );
+            text.attr( { x : x2, y : y1 + 20, textContent : m + ( n * i ) } );
+            text.attr( { 'class' : 'graph-axis-interval-text graph-axis-x-interval-text' } );
+            
+            this.__canvas__.appendChild( text );
+            
+        },
+        
+        /**
+         *
+         * FIXME : Refactor code.
+         * 
+         */
+        
+        renderAxisY: function( d, range, i ) {
+            
+            var p = [ 40, 40, 40, 40 ];
+            
+            var w = d.width - p[1] - p[3];
+            var h = d.height - p[0] - p[2];
+            
+            var line = new phi.dom.svg.SVGShapeElement( 'line' );
+            line.attr( { x1 : p[3], y1: p[0], x2 : p[3], y2 : h + p[0] } );
+            line.attr( { 'class' : 'graph-axis graph-axis-y' } );
+            
+            this.__canvas__.appendChild( line );
+            
+            var l = range.delta / i;
+            var s = h / l;
+            var m = range.min;
+            
+            for ( var n = 0; n < l+1; n++) {
+                this.renderAxisYInterval( p, w, h, l, s, i, n, m );
+            }
+            
+        },
+        
+        /**
+         *
+         * FIXME : refactor
+         * FIXME : deal with large numbers on intervals
+         *
+         */
+        
+        renderAxisYInterval: function( p, w, h, l, s, i, n, m ) {
+            
+            var x1 = p[3],
+                y1 = p[0] + h - ( n * s ),
+                x2 = p[3] + w,
+                y2 = y1;
+            
+            var line = new phi.dom.svg.SVGShapeElement( 'line' );
+            line.attr( { x1 : x1, y1 : y1, x2 : x2, y2: y2 } );
+            line.attr( { 'class' : 'graph-axis-interval graph-axis-interval-' + n } );
+            
+            this.__canvas__.appendChild( line );
+            
+            var text = new phi.dom.svg.SVGShapeElement( 'text' );
+            text.attr( { x : x1 - 20, y : y2 + 3, textContent : m + ( n * i ) } );
+            text.attr( { 'class' : 'graph-axis-interval-text graph-axis-y-interval-text' } );
+            
+            this.__canvas__.appendChild( text );
+            
+        },
+        
+        stretchRangeToFit : function( range, interval ) {
+            
+            var min = Math.floor( range.min / interval ) * interval, 
+                max = Math.ceil( range.max / interval ) * interval;
+            
+            return {
+                min : min, max : max,
+                delta : max - min
+            }
+            
+        },
+        
+        /**
+         *
+         * FIXME : Refactor code.
+         * 
+         */
+        
+        resolveAxisInterval : function( delta, exponent, closest, factor ) {
+            
+            var optimal = 10;
+            var proper = [ 1, 2, 5 ];
+            
+            exponent = exponent || 1;
+            closest  = closest || Infinity;
+            
+            var f, p;
+            for ( var i = 0; i < proper.length; i++ ) {
+                
+                f = proper[ i ] * exponent;
+                p = this.resolveAxisIntervalProximity( optimal, delta / f );
+                
+                if ( p < closest ) {
+                    closest = p;
+                    factor = f;
+                } else {
+                    return factor;
+                }
+                
+            }
+            
+            return this.resolveAxisInterval( delta, exponent * 10, closest, factor );
+            
+        },
+        
+        resolveAxisIntervalProximity : function( optimal, real ) {
+            return Math.abs( optimal - real );
+        }
+        
+    });
+    
+    LineGraph.DEFAULTS = {
+        'axis-x-name'       : undefined,
+        'axis-x-interval'   : 10,
+        'axis-y-name'       : undefined,
+        'axis-y-interval'   : 10,
+        'point-color'       : '#ff0099',
+        'point-color-shift' : 0
+    };
+    
+} )( phi.dom );
