b6b3048fc78a1c04c9d0c973cdb6b3a645f695d0
Aleksandar Dukleski <a.dukleski@gmail.com>
2015-03-21 12:08:58 +0100

    Add event listeners

--- phi-parent/phi-js/phi-js-date/src/main/resources/static/js/lib/phi/phi.date.js
+++ phi-parent/phi-js/phi-js-date/src/main/resources/static/js/lib/phi/phi.date.js
@@ -28,46 +28,96 @@
  */
 
 (function( phi, dom, form ) {
-    
+
     phi.date = {};
-    
-    /*
+     /**
      *
      * 
      *
      */
     
-    var PhiDate = phi.date.Calendar = phi({
+    var Calendar = phi.date.Calendar = phi({
         
         __extends__: phi.EventTarget,
         
         __init__: function( node, options ) {
-            
-            this.__node__       = node;
-            this.__options__    = phi.extend( {}, PhiDate.DEFAULTS, options );
-            
-        }
 
+            this.__node__       = node;
+            this.__options__    = phi.extend( {}, Calendar.DEFAULTS, options );
+            // this.__nodeForm__   = new form.Form(node);
+            this.__calendar__   = {};
+
+            this.buildCalendar( this.__options__ );
+            this.createEventListeners( this.__node__, this.__calendar__ );
+        },
+
+        /**
+        *
+        *
+        */        
+
+        buildCalendar: function ( options ) {
+            this.__calendar__ = document.createElement('div');
+            this.__calendar__.setAttribute('class', 'phi-calendar');
+
+            document.body.appendChild( this.__calendar__ );
+        },
+
+        /**
+        *
+        *
+        **/
         
+        createEventListeners: function ( node, calendar ) {
+            node.addEventListener( 'focus',        this.handleFocus.bind(this), true );
+            node.addEventListener( 'blur',         this.handleBlur.bind(this), true );
+        },
+
+        /**
+         *
+         * Handle input focus
+         *
+         */
+
+        handleFocus: function( e ) {
+
+            this.__calendar__.setAttribute('class', this.__calendar__.getAttribute('class').replace(' active', '') + ' active');
+
+            // this.dispatchEvent( { type : 'inputfocus', explicitOriginalTarget : e.target } );
+        },
+
+        /**
+        *
+        * Handle input blur
+        *
+        */
+        
+        handleBlur: function ( e ) {
+            // this.dispatchEvent( { type : 'inputblur',  explicitOriginalTarget : e.target } );
+        }
     } );
 
-    PhiDate.KEY_CODE = {
-        BACKSPACE: 8,
-        COMMA: 188,
-        DELETE: 46,
-        DOWN: 40,
-        END: 35,
-        ENTER: 13,
-        ESCAPE: 27,
-        HOME: 36,
-        LEFT: 37,
-        PAGE_DOWN: 34,
-        PAGE_UP: 33,
-        PERIOD: 190,
-        RIGHT: 39,
-        SPACE: 32,
-        TAB: 9,
-        UP: 38
+    Calendar.KEY_CODE = {
+        BACKSPACE : 8,
+        COMMA     : 188,
+        DELETE    : 46,
+        DOWN      : 40,
+        END       : 35,
+        ENTER     : 13,
+        ESCAPE    : 27,
+        HOME      : 36,
+        LEFT      : 37,
+        PAGE_DOWN : 34,
+        PAGE_UP   : 33,
+        PERIOD    : 190,
+        RIGHT     : 39,
+        SPACE     : 32,
+        TAB       : 9,
+        UP        : 38
+    };
+
+    Calendar.DEFAULTS = {
+
     };
     
     
